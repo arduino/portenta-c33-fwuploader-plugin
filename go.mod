a07999d0526da62d7d21eb5b78c9984069e5e171
MatteoPologruto <m.pologruto@ext.arduino.cc>
2023-07-25 12:16:32 +0200

    Add firmware flash function

--- go.mod
+++ go.mod
@@ -3,16 +3,21 @@ module github.com/arduino/portenta-c33-fwuploader-plugin
 go 1.20
 
 require (
+	github.com/arduino/arduino-cli v0.0.0-20230724101917-62bb4c47cb2b
 	github.com/arduino/fwuploader-plugin-helper v1.0.0
 	github.com/arduino/go-paths-helper v1.9.1
 	go.bug.st/relaxed-semver v0.11.0
+	go.bug.st/serial v1.3.2
+	golang.org/x/exp v0.0.0-20230711153332-06a737ee72cb
 )
 
 require (
+	github.com/creack/goselect v0.1.2 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/leonelquinteros/gotext v1.4.0 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/spf13/cobra v1.7.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
-	golang.org/x/exp v0.0.0-20230711153332-06a737ee72cb // indirect
+	golang.org/x/sys v0.6.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -18,30 +18,58 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"time"
 
+	"github.com/arduino/arduino-cli/executils"
 	helper "github.com/arduino/fwuploader-plugin-helper"
 	"github.com/arduino/go-paths-helper"
+	"github.com/arduino/portenta-c33-fwuploader-plugin/serial"
 	semver "go.bug.st/relaxed-semver"
+	"golang.org/x/exp/slog"
 )
 
-type dummyPlugin struct {
+const (
+	pluginName = "portenta-c33-fwuploader"
+)
+
+var (
+	versionString = "0.0.0-git"
+)
+
+var sketch = paths.New("./C3SerialPassthrough.ino.bin")
+
+type portentaC33Plugin struct {
+	esptoolBin *paths.Path
+	dfuUtilBin *paths.Path
 }
 
 func main() {
-	helper.RunPlugin(&dummyPlugin{})
+	esptoolPath, err := helper.FindToolPath("esptool", semver.MustParse("3.3.2"))
+	if err != nil {
+		log.Fatalln("Couldn't find esptool@3.3.2 binary")
+	}
+	dfuUtilPath, err := helper.FindToolPath("dfu-util", semver.MustParse("0.11.0-arduino5"))
+	if err != nil {
+		log.Fatalln("Couldn't find dfu-util@0.11.0-arduino5 binary")
+	}
+
+	helper.RunPlugin(&portentaC33Plugin{
+		esptoolBin: esptoolPath.Join(("esptool")),
+		dfuUtilBin: dfuUtilPath.Join("dfu-util"),
+	})
 }
 
 // GetPluginInfo returns information about the plugin
-func (d *dummyPlugin) GetPluginInfo() *helper.PluginInfo {
+func (d *portentaC33Plugin) GetPluginInfo() *helper.PluginInfo {
 	return &helper.PluginInfo{
-		Name:    "dummy",
-		Version: semver.MustParse("1.0.0"),
+		Name:    pluginName,
+		Version: semver.MustParse(versionString),
 	}
 }
 
 // UploadFirmware performs a firmware upload on the board
-func (d *dummyPlugin) UploadFirmware(portAddress, fqbn string, firmwarePath *paths.Path, feedback *helper.PluginFeedback) error {
+func (d *portentaC33Plugin) UploadFirmware(portAddress, fqbn string, firmwarePath *paths.Path, feedback *helper.PluginFeedback) error {
 	if portAddress == "" {
 		fmt.Fprintln(feedback.Err(), "Port address not specified")
 		return fmt.Errorf("invalid port address")
@@ -54,10 +82,7 @@ func (d *dummyPlugin) UploadFirmware(portAddress, fqbn string, firmwarePath *pat
 
 	// Providing the fqbn to the plugin allows us to support a family of boards instead of a single one
 	switch fqbn {
-	case "arduino:renesas_uno:unor5":
-		// Do some board specific operations here
-		fmt.Fprintf(feedback.Out(), "Uploading firmware for %s \n", fqbn)
-	case "arduino:renesas_uno:unor4wifi":
+	case "arduino:renesas_portenta:portenta_c33":
 		// Do some board specific operations here
 		fmt.Fprintf(feedback.Out(), "Uploading firmware for %s \n", fqbn)
 	default:
@@ -65,15 +90,26 @@ func (d *dummyPlugin) UploadFirmware(portAddress, fqbn string, firmwarePath *pat
 		return fmt.Errorf("invalid fqbn")
 	}
 
-	// Fake upload
-	time.Sleep(5 * time.Second)
+	if err := d.reboot(&portAddress, feedback); err != nil {
+		return err
+	}
+
+	cmd, err := executils.NewProcess([]string{}, d.esptoolBin.String(), "--chip", "esp32c3", "-p", portAddress, "-b", "230400", "--before=default_reset", "--after=hard_reset", "write_flash", "--flash_mode", "dio", "--flash_freq", "80m", "--flash_size", "4MB", "0x0", firmwarePath.String())
+	if err != nil {
+		return err
+	}
+	cmd.RedirectStderrTo(feedback.Err())
+	cmd.RedirectStdoutTo(feedback.Out())
+	if err := cmd.Run(); err != nil {
+		return err
+	}
 
 	fmt.Fprintf(feedback.Out(), "Upload completed!\n")
 	return nil
 }
 
 // UploadCertificate performs a certificate upload on the board
-func (d *dummyPlugin) UploadCertificate(portAddress, fqbn string, certificatePath *paths.Path, feedback *helper.PluginFeedback) error {
+func (d *portentaC33Plugin) UploadCertificate(portAddress, fqbn string, certificatePath *paths.Path, feedback *helper.PluginFeedback) error {
 	if portAddress == "" {
 		fmt.Fprintln(feedback.Err(), "Port address not specified")
 		return fmt.Errorf("invalid port address")
@@ -88,7 +124,7 @@ func (d *dummyPlugin) UploadCertificate(portAddress, fqbn string, certificatePat
 }
 
 // GetFirmwareVersion retrieve the firmware version installed on the board
-func (d *dummyPlugin) GetFirmwareVersion(portAddress, fqbn string, feedback *helper.PluginFeedback) (*semver.RelaxedVersion, error) {
+func (d *portentaC33Plugin) GetFirmwareVersion(portAddress, fqbn string, feedback *helper.PluginFeedback) (*semver.RelaxedVersion, error) {
 	if portAddress == "" {
 		fmt.Fprintln(feedback.Err(), "Port address not specified")
 		return nil, fmt.Errorf("invalid port address")
@@ -100,4 +136,49 @@ func (d *dummyPlugin) GetFirmwareVersion(portAddress, fqbn string, feedback *hel
 
 	fmt.Fprintf(feedback.Out(), "Done!\n")
 	return semver.ParseRelaxed("1.0.0"), nil
+}
+
+func (d *portentaC33Plugin) reboot(portAddress *string, feedback *helper.PluginFeedback) error {
+	// Will be used later to check if the OS changed the serial port.
+	allSerialPorts, err := serial.AllPorts()
+	if err != nil {
+		return err
+	}
+
+	if err := d.uploadCommandsSketch(*portAddress, feedback); err != nil {
+		return fmt.Errorf("upload commands sketch: %v", err)
+	}
+
+	fmt.Fprintf(feedback.Out(), "\nWaiting to flash the binary...\n")
+
+	slog.Info("check if serial port has changed")
+	// When a board is successfully rebooted in esp32 mode, it might change the serial port.
+	// Every 250ms we're watching for new ports, if a new one is found we return that otherwise
+	// we'll wait the 10 seconds timeout expiration.
+	newPort, changed, err := allSerialPorts.NewPort()
+	if err != nil {
+		return err
+	}
+	if changed {
+		*portAddress = newPort
+	}
+	return nil
+}
+
+func (d *portentaC33Plugin) uploadCommandsSketch(portAddress string, feedback *helper.PluginFeedback) error {
+	slog.Info("upload_command_sketch")
+
+	slog.Info("uploading command sketch with dfu-util")
+	cmd, err := executils.NewProcess([]string{}, d.dfuUtilBin.String(), "--device", "0x2341:0x0068,:0x0368", "-D", sketch.String(), "-a0", "-Q")
+	if err != nil {
+		return err
+	}
+	cmd.RedirectStderrTo(feedback.Err())
+	cmd.RedirectStdoutTo(feedback.Out())
+	if err := cmd.Run(); err != nil {
+		return err
+	}
+
+	time.Sleep(1 * time.Second)
+	return nil
 }
